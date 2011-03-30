27a6616ef0d00577894d4fcfd69616e74017dbab
Jamie <jamie@intalio.com>
2011-03-30 10:30:41 +0800

    Fix BPM-893

--- src/main/java/com/intalio/web/server/UUIDBasedRepositoryServlet.java
+++ src/main/java/com/intalio/web/server/UUIDBasedRepositoryServlet.java
@@ -262,6 +262,8 @@ public class UUIDBasedRepositoryServlet extends HttpServlet {
             String uuid = (String) jsonObject.get("uuid");
             String profileName = (String) jsonObject.get("profile");
             boolean autosave = jsonObject.getBoolean("savetype");
+            
+            json = json.replaceAll("<", "&lt;");
 
             if (_logger.isDebugEnabled()) {
                 _logger.debug("Calling UUIDBasedRepositoryServlet doPost()...");
